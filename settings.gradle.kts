fc487b0b5186b17ba5bee82978f9676fd67a04ab
ivanadito-ap <ivanaditoap@gmail.com>
2026-01-08 19:58:24 +0700

    Revert "Update architectury-plugin version"
    This reverts commit 37b8b716f91d38980924772dd385b562c0d88371.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,27 @@
 pluginManagement {
     repositories {
-        maven("https://maven.fabricmc.net/")
-        maven("https://maven.architectury.dev/")
-        maven("https://maven.minecraftforge.net/")
+        exclusiveContent {
+            forRepository { maven("https://maven.architectury.dev") }
+            filter {
+                includeGroup("architectury-plugin")
+                includeGroupByRegex("dev\\.architectury.*")
+                includeGroup("com.mojang")
+            }
+        }
+        exclusiveContent {
+            forRepository { maven("https://maven.fabricmc.net") }
+            filter {
+                includeGroup("net.fabricmc")
+                includeGroup("fabric-loom")
+            }
+        }
+        exclusiveContent {
+            forRepository { maven("https://maven.minecraftforge.net") }
+            filter {
+                includeGroupByRegex("net\\.minecraftforge.*")
+                includeGroup("de.oceanlabs.mcp")
+            }
+        }
         mavenCentral()
         gradlePluginPortal()
     }
