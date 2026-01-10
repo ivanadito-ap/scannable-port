901008ba498d47c58a5d8636046e0b0b4b656809
ivanadito-ap <ivanaditoap@gmail.com>
2026-01-10 19:15:51 +0700

    Update LevelRendererMixin

--- common/src/main/java/li/cil/scannable/mixin/client/LevelRendererMixin.java
+++ common/src/main/java/li/cil/scannable/mixin/client/LevelRendererMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.client.Camera;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.LightTexture;
+import org.joml.Matrix3f;
 import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,8 +15,32 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LevelRenderer.class)
 public class LevelRendererMixin {
-    @Inject(method = "renderLevel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/OutlineBufferSource;endOutlineBatch()V", shift = At.Shift.BEFORE))
-    private void renderLevel(final PoseStack poseStack, final float tickDelta, final long nanos, final boolean shouldRenderBlockOutline, final Camera camera, final GameRenderer gameRenderer, final LightTexture lightTexture, final Matrix4f projectionMatrix, final CallbackInfo ci) {
+    @Inject(
+        method = "renderLevel",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/renderer/OutlineBufferSource;endOutlineBatch()V",
+            shift = At.Shift.BEFORE
+        )
+    )
+    private void renderLevel(
+        final float tickDelta,
+        final long nanos,
+        final boolean shouldRenderBlockOutline,
+        final Camera camera,
+        final GameRenderer gameRenderer,
+        final LightTexture lightTexture,
+        final Matrix4f frustumMatrix,
+        final Matrix4f projectionMatrix,
+        final CallbackInfo ci
+    ) {
+        final PoseStack poseStack = new PoseStack();
+        poseStack.last().pose().set(frustumMatrix);
+
+        final Matrix3f normal = new Matrix3f(frustumMatrix);
+        normal.invert().transpose();
+        poseStack.last().normal().set(normal);
+
         ScannerRenderer.render(poseStack);
     }
 }
