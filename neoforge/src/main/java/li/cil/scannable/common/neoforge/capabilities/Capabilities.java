18e377719bdd46576cafe3fb6f9ba94742587a17
ivanadito-ap <ivanaditoap@gmail.com>
2026-01-10 17:41:41 +0700

    Fix Capabilities to use registry context #2

--- neoforge/src/main/java/li/cil/scannable/common/neoforge/capabilities/Capabilities.java
+++ neoforge/src/main/java/li/cil/scannable/common/neoforge/capabilities/Capabilities.java
@@ -5,6 +5,8 @@ import li.cil.scannable.common.energy.neoforge.ScannerEnergyStorage;
 import li.cil.scannable.common.inventory.ScannerContainer;
 import li.cil.scannable.common.item.Items;
 import li.cil.scannable.common.item.ScannerModuleItem;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -16,19 +18,27 @@ import net.neoforged.neoforge.items.wrapper.InvWrapper;
 
 @EventBusSubscriber(modid = API.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
 public final class Capabilities {
-    public static final class ScannerModule {
-        public static final ItemCapability<li.cil.scannable.api.scanning.ScannerModule, Void> ITEM = ItemCapability.createVoid(new ResourceLocation(API.MOD_ID, "scanner_module"), li.cil.scannable.api.scanning.ScannerModule.class);
-    }
 
-    // --------------------------------------------------------------------- //
+    private static final RegistryAccess BUILTIN_ACCESS =
+        RegistryAccess.fromRegistryOfRegistries(BuiltInRegistries.REGISTRY);
+
+    public static final class ScannerModule {
+        public static final ItemCapability<li.cil.scannable.api.scanning.ScannerModule, Void> ITEM =
+            ItemCapability.createVoid(new ResourceLocation(API.MOD_ID, "scanner_module"),
+                li.cil.scannable.api.scanning.ScannerModule.class);
+    }
 
     @SubscribeEvent
     public static void initialize(final RegisterCapabilitiesEvent event) {
-        event.registerItem(ItemHandler.ITEM, (stack, context) -> new InvWrapper(ScannerContainer.of(stack, context.level().registryAccess())),
+        event.registerItem(ItemHandler.ITEM, (stack, unused) ->
+                new InvWrapper(ScannerContainer.of(stack, BUILTIN_ACCESS)),
             Items.SCANNER.get());
-        event.registerItem(EnergyStorage.ITEM, (stack, context) -> ScannerEnergyStorage.of(stack),
+
+        event.registerItem(EnergyStorage.ITEM, (stack, unused) -> ScannerEnergyStorage.of(stack),
             Items.SCANNER.get());
-        event.registerItem(ScannerModule.ITEM, (stack, context) -> ((ScannerModuleItem) stack.getItem()).getModule(),
+
+        event.registerItem(ScannerModule.ITEM, (stack, unused) ->
+                ((ScannerModuleItem) stack.getItem()).getModule(),
             Items.RANGE_MODULE.get(),
             Items.ENTITY_MODULE.get(),
             Items.FRIENDLY_ENTITY_MODULE.get(),
@@ -40,3 +50,4 @@ public final class Capabilities {
             Items.CHEST_MODULE.get());
     }
 }
+
